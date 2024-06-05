255888f1d823c2ad24b81dcb8e7f744457df6541
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-05 17:47:01 +0200

    fixed code issue

--- js/script.js
+++ js/script.js
@@ -3,20 +3,31 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
+            mail: [],
             response : ''
         }
     },
     methods:{
-        getData(){
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-            .then((response) => {
-                console.log(response);
-                this.response = response.data.response;
-            });
-        }
-    },
 
+        getData() {
+            for (let i = 0; i < 10; i++ ) {
+                
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((response) => {
+                    console.log(response);
+                    this.mail.push(response.data.response);
+                });
+                
+                
+            }
+        },
+    },
+    
     created(){
         this.getData();
     },
 }).mount('#app')
+
+
+console.log(mail);
+
