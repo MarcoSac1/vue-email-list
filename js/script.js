6395701d3d7fd3938775e2a7a33f6fc3fd061ff5
MarcoSac1 <marcosaccone8@gmail.com>
2024-06-05 16:19:03 +0200

    created axios.get to use api

--- js/script.js
+++ js/script.js
@@ -3,10 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            
+            response : ''
         }
     },
     methods:{
-        
-    }
+        getData(){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                console.log(response);
+                this.response = response.data.response;
+            });
+        }
+    },
+
+    created(){
+        this.getData();
+    },
 }).mount('#app')
